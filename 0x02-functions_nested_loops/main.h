3d77085f5a1464cd785291e510ac932868a53285
Your Name <you@example.com>
2023-03-21 08:17:11 -0700

    0-putchar

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,1 +1,23 @@
 int _putchar(char);
+
+void print_alphabet(void);
+
+int _islower(int);
+
+int _isalpha(int);
+
+int print_sign(int);
+
+int _abs(int);
+
+int print_last_digit(int);
+
+void jack_bauer(void);
+
+void times_table(void);
+
+int add(int, int);
+
+void print_to_98(int);
+
+void print_times_table(int);
