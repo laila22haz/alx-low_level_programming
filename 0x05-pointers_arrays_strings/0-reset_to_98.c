199255b70eec98d5e20e360c4bd62399fb7e2ffd
Your Name <you@example.com>
2023-03-27 09:34:59 -0700

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * main - updates the value of the variable
+ * reset_to_98 - updates the value of the variable
  * the pointer points to to 98
  * @n: pointer to the variable to update
  */
