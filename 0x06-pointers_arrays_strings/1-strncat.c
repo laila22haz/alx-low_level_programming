e3165078f3b7d217632aa60067d28670f71706f6
Your Name <you@example.com>
2023-03-29 10:55:14 -0700

    1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,27 +1,30 @@
 #include "main.h"
 /**
- * _strncpy - copy a string
+ * _strncat - concatenate two strings
+ * using at most n bytes from src
  * @dest: input value
  * @src: input value
  * @n: input value
  *
  * Return: dest
  */
-char *_strncpy(char *dest, char *src, int n)
+char *_strncat(char *dest, char *src, int n)
 {
+	int i;
 	int j;
 
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
 	j = 0;
 	while (j < n && src[j] != '\0')
 	{
-		dest[j] = src[j];
-		j++;
+	dest[i] = src[j];
+	i++;
+	j++;
 	}
-	while (j < n)
-	{
-		dest[j] = '\0';
-		j++;
-	}
-
+	dest[i] = '\0';
 	return (dest);
 }
