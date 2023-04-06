d73d1a20be886679c1f3a7ee14477d8cb78b3262
Your Name <you@example.com>
2023-04-06 13:21:17 -0700

    6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -2,9 +2,24 @@
 
 /*laila22haz*/
 /**
- *is_prime_number - if a prime number
- *@n: number integer
- *Return: value
+ * prime2 - find prime number
+ * @x: integer
+ * @y: integer
+ * Return: value
+ */
+int prime2(int x, int y)
+{
+	if (y == x)
+	return (1);
+	else if (y % x == 0)
+	return (0);
+	return (prime2(x + 1, y));
+}
+
+/**
+ * is_prime_number - if a prime number
+ * @n: number integer
+ * Return: value
  *
  *
  */
@@ -13,19 +28,4 @@ int is_prime_number(int n)
 	if (n < 0 || n == 1)
 		return (0);
 	return (prime2(2, n));
-}
-
-/**
- *prime2 - find prime number
- *@x: integer
- *@y: integer
- *Return: value
- */
-int prime2(int x, int y)
-{
-	if (y == x)
-		return (1);
-	else if (y % x == 0)
-		return (0);
-	return (prime2(x + 1, y));
 }
