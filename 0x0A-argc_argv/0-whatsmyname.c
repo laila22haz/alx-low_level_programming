e6ef5d549e9a20609a92e98ba61430ba7edf560b
Your Name <you@example.com>
2023-04-08 21:20:30 -0700

    mynewnameis

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,14 +3,16 @@
 
 /*laila22haz*/
 /**
- *  main - program that prints its name
- *  
- * Return 0 
+ * main - program that prints its name
+ * @argc : the first argument
+ * @argv : the second argument
+ * Return: 0
  */
 int main(int argc, char *argv[])
 {
 	int x;
-	for(x = 0;x < argc;x++)
-	printf("%s \n",argv[x]);
+
+	for (x = 0; x < argc; x++)
+	printf("%s \n", argv[x]);
 	return (0);
 }
