85d78705eb0f46e3886634d56c0d937df0172e64
Your Name <you@example.com>
2023-04-09 15:33:33 -0700

    2-args.c

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -3,7 +3,7 @@
 
 /*laila22haz*/
 /**
- * main - program that prints its name
+ * main - program that prints all arguments it receives
  * @argc : the first argument
  * @argv : the second argument
  * Return: 0
