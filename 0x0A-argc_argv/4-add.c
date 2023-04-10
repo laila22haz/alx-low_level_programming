8cc0caae12150cdbd2c8abca9aa9efe1f241e978
Your Name <you@example.com>
2023-04-10 13:51:46 -0700

    3-mul.c

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - program that adds positive numbers
+ * @argc: number arguments
+ * @argv: array arguments
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int result;
+	int num1, num2;
+
+	
