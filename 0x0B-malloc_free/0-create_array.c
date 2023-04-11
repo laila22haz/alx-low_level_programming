a354446ccaf688220821dc4295711553f12f03d5
Your Name <you@example.com>
2023-04-11 15:35:44 -0700

    fixed

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+
+/**
+ * create_array - program that creates an array of chars
+ * @size : the first argument
+ * @c : the second argument
+ *
+ * Return: arr
+ */
+char *create_array(unsigned int size, char c)
+{
+	if (size == 0)
+	{
+	return (NULL);
+	}
+	char *arr = malloc(sizeof(char) * size);
+
+        if (arr == NULL)
+	{
+	return (NULL);
+	}
+	for (unsigned int i = 0; i < size; i++)
+	{
+	arr[i] = c;
+	}
+	return (arr);
+}
