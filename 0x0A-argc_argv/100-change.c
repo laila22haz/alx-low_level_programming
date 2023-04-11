e84f1780fe00fc51f1c1aa80e5e069f5420f1fb4
Your Name <you@example.com>
2023-04-10 20:04:56 -0700

    100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,42 +1,37 @@
 #include <stdio.h>
-#include <ctype.h>
 #include <stdlib.h>
 
 /**
- * main - program that adds positive numbers
- * @argc: number arguments
- * @argv: array arguments
+ * main -  program that prints the minimum number of coins to make change .
+ * @argc: arguments 1.
+ * @argv: arguments 2.
  *
- * Return: 0
+ * Return: if the number of arguments passed to your program is not 1 -1.
+ *         Otherwise - 0.
  */
 int main(int argc, char *argv[])
 {
-	int i;
-	int cents = atoi(argv[1]);
+	int cents, num_coins, i, count;
 	int coins[] = {25, 10, 5, 2, 1};
-	int num_coins = sizeof(coins) / sizeof(coins[0]);
-	int count = 0;
 
-	if (argc != 0)
+	if (argc != 2)
 	{
 	printf("Error\n");
 	return (1);
 	}
+	cents = atoi(argv[1]);
 	if (cents < 0)
 	{
 	printf("0\n");
 	return (0);
 	}
+	num_coins = sizeof(coins) / sizeof(coins[0]);
+	count = 0;
 	for (i = 0; i < num_coins; i++)
 	{
-	while (cents >= coins[i])
-	{
-		count++;
-		cents -= coins[i];
+	count += cents / coins[i];
+	cents %= coins[i];
 	}
-	}
-
 	printf("%d\n", count);
 	return (0);
 }
-
