e4d7f7aa30427aac76d6d27a95e0b826994ef88e
Your Name <you@example.com>
2023-04-17 12:43:06 -0700

    1-init_dog.c

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "dog.h"
+
+/*laila22haz*/
+/**
+ * init_dog - Short description
+ * @d: First member
+ * @name: Second member
+ * @age: Third member
+ * @owner: fourth member
+ *
+ * Description: Longer description
+ * Return: d
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d != NULL)
+
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
