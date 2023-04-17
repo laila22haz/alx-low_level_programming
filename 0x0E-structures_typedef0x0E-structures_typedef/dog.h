7e3304602e88e10297d69ab5fa0e81c2f012f5b2
Your Name <you@example.com>
2023-04-17 09:35:25 -0700

    dog.h

--- 0x0E-structures_typedef0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef0x0E-structures_typedef/dog.h
@@ -10,12 +10,12 @@
  *
  * Description: store different data types
  */
-typedef struct dog
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-} dog_t;
+};
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
