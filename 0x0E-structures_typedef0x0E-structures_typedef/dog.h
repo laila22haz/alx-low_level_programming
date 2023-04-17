7421584c39a59b7fd652d3dcdeea413e9455943d
Your Name <you@example.com>
2023-04-17 08:41:23 -0700

    dog.h

---
+++ 0x0E-structures_typedef0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * struct dog - structure that Define a new type
+ * @name : the first element
+ * @age : the second element
+ * @owner : the third element
+ *
+ * Description: Longer description
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
