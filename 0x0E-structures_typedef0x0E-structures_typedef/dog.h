862b1e3479cac3c3f6e035acfdd6b05263d313f5
Your Name <you@example.com>
2023-04-17 09:05:05 -0700

    dog.h

--- 0x0E-structures_typedef0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef0x0E-structures_typedef/dog.h
@@ -1,29 +1,25 @@
-#include <stdio.h>
+#ifndef DOG_H
+#define DOG_H
 
+/*laila22haz*/
 /**
- * struct dog - structure that Define a new type
- * @name : the first element
- * @age : the second element
- * @owner : the third element
+ * struct dog - struct with dog info
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
  *
- * Description: Longer description
+ * Description: store different data types
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
-
-/**
- * dog_t - Typedef for unsigned int
- */
-typedef struct dog dog_t;
+} dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
-
 void print_dog(struct dog *d);
-
 dog_t *new_dog(char *name, float age, char *owner);
-
 void free_dog(dog_t *d);
+
+#endif
