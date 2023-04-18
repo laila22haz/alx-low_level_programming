35d2d530a53db3f7c224df8d77ecbb79a654fc8a
Your Name <you@example.com>
2023-04-17 17:32:13 -0700

    0-object_like_macro.h

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,6 @@
+#ifndef _OBJECT_LIKE_MACRO
+#define _OBJECT_LIKE_MACRO
+
+#define SIZE 1024
+
+#endif
---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+#define BLURT printf ("%s\n", __FILE__)
+/*laila22haz*/
+/**
+ * main - print the name of the file it was compiled from
+ *
+ * Return: 0
+ */
+void help_function ()
+{
+	        BLURT;
+}
+
+int main ()
+{
+	BLURT;
+	help_function ();
+	return (0);
+}
