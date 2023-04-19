2a7290293ea942405f8358769f17a493dfe89a45
Your Name <you@example.com>
2023-04-19 11:48:15 -0700

    3-main.c, 3-op_functions.c, 3-get_op_func.c, 3-calc.h

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "3-calc.h"
+
+/**
+ * get_op_func - perform the operation asked by the user
+ * @s : argument
+ *
+ * Return: 0
+ */
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i = 0;
+
+	while (ops[i].op)
+	{
+		if (*(ops[i].op) == *s)
+			return (ops[i].f);
+		i++;
+	}
+	return (NULL);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+
+/**
+ * op_add - addition
+ * @a : the first argument
+ * @b : the second argument
+ *
+ * Return: 0
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - addition
+ * @a : the first argument
+ * @b : the second argument
+ *
+ * Return: 0
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - addition
+ * @a : the first argument
+ * @b : the second argument
+ *
+ * Return: 0
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - addition
+ * @a : the first argument
+ * @b : the second argument
+ *
+ * Return: 0
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - addition
+ * @a : the first argument
+ * @b : the second argument
+ *
+ * Return: 0
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
