3b35ff245e909787db3e5eb0a4b7e3ddb5061622
laila22haz <hazmirlaila316@gmail.com>
2023-04-27 15:56:50 -0700

    linked lists

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -5,7 +5,7 @@
  * print_list - function that prints all the elements of a list_t list
  * @h : the first argument
  *
- * Return: size
+ * Return: the number of nodes
  */
 size_t print_list(const list_t *h)
 {
