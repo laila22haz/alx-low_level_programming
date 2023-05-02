e5cec7284bab29485a19419c6eef589e3c8b7d5c
laila22haz <hazmirlaila316@gmail.com>
2023-05-01 18:32:47 -0700

    linked lists

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -11,10 +11,11 @@ void free_listint2(listint_t **head)
 {
 	listint_t *tmp;
 
-	while (*head == NULL)
+	while (*head != NULL)
 	{
 		tmp = *head;
 		*head = (*head)->next;
 		free(tmp);
 	}
+	head = NULL;
 }
