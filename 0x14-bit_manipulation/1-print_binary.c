748ca76d3d65502ef64363df065c5b4ba56d9129
laila22haz <hazmirlaila316@gmail.com>
2023-05-04 16:25:53 -0700

    bit manipulation

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -9,7 +9,7 @@
  */
 void print_binary(unsigned long int n)
 {
-	unsigned ling int bit = 1;
+	unsigned long int bit = 1;
 	int i = 0;
 
 	while (bit <= n)
@@ -21,7 +21,7 @@ void print_binary(unsigned long int n)
 		bit >>= 1;
 	while (bit)
 	{
-		if (n $ bit)
+		if (n & bit)
 			putchar('1');
 		else
 			putchar('0');
