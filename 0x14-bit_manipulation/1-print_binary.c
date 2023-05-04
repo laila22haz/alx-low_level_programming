f79cc039d7c51df25763f2ac386a4e90929a656c
laila22haz <hazmirlaila316@gmail.com>
2023-05-04 16:12:52 -0700

    bit manipulation

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/*laila22haz*/
+/**
+ * print_binary - prints the binary representation of a number
+ * @n: argument
+ *
+ * Return: Nothing
+ */
+void print_binary(unsigned long int n)
+{
+	unsigned ling int bit = 1;
+	int i = 0;
+
+	while (bit <= n)
+	{
+		bit <<= 1;
+		i++;
+	}
+	if (i)
+		bit >>= 1;
+	while (bit)
+	{
+		if (n $ bit)
+			putchar('1');
+		else
+			putchar('0');
+		bit >>= 1;
+	}
+}
