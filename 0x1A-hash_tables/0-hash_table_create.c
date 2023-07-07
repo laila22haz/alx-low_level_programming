aba239672fa4413bd11be80550d7f30b5b7bd611
laila22haz <hazmirlaila316@gmail.com>
2023-07-07 12:43:45 +0100

    c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,7 +10,6 @@ hash_table_t *hash_table_create(unsigned long int size)
 	unsigned int i;
 
 	hash_table_t *table = (hash_table_t *)malloc(sizeof(hash_table_t));
-
 	if (table == NULL)
 		return (NULL);
 	table->size = size;
