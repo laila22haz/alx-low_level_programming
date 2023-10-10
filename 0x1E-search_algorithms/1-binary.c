0fcb5ac2226365986d840521ef858558b35ff861
laila22haz <hazmirlaila316@gmail.com>
2023-10-10 19:05:07 +0100

    task 1

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -15,7 +15,7 @@ int binary_search(int *array, size_t size, int value)
 	while (left <= right)
 	{
 		midle = left + (right - left) / 2;
-		printf("Searching in subarray: ");
+		printf("Searching in array: ");
 		for (i = left; i <= right; i++)
 		{
 			printf("%d", array[i]);
